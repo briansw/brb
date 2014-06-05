f79039a9d6d90564b3278299a9b9bb8b8fddaff4
Jack Jennings <j@ckjennin.gs>
2014-06-05 11:36:29 -0700

    switch over to rspec for tests

--- lib/brb/engine.rb
+++ lib/brb/engine.rb
@@ -1,4 +1,18 @@
 module Brb
   class Engine < ::Rails::Engine
+    
+    config.generators do |g|
+      g.test_framework :rspec, 
+        fixture:          false,
+        view_specs:       false,
+        helper_specs:     false,
+        routing_specs:    false,
+        controller_specs: true,
+        request_specs:    false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.assets false
+      g.helper false
+    end
+    
   end
 end

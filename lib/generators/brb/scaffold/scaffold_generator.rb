844f84f32c3842646a3b74a62c22ea1494941ba8
Jack Jennings <j@ckjennin.gs>
2014-06-07 12:58:06 -0700

    add admin routes with scaffold generator

--- lib/generators/brb/scaffold/scaffold_generator.rb
+++ lib/generators/brb/scaffold/scaffold_generator.rb
@@ -1,3 +1,7 @@
 class Brb::ScaffoldGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
+  
+  def add_admin_route
+    route "admin_for :#{file_name.pluralize}"
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,3 +13,19 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 if ActiveSupport::TestCase.method_defined?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
 end
+
+module GeneratorsTestHelper
+  def self.included(base)
+    base.class_eval do
+      destination File.join(Rails.root, "tmp")
+      setup :prepare_destination
+    end
+  end
+
+  def copy_routes
+    routes = File.expand_path("../templates/routes.rb", __FILE__)
+    destination = File.join(destination_root, "config")
+    FileUtils.mkdir_p(destination)
+    FileUtils.cp routes, destination
+  end
+end

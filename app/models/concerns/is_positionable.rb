2091b7a0da0d74d128e0c8b4213ea5cfb518eec8
Brian Watterson <briansw@gmail.com>
2014-06-17 17:09:41 -0400

    add is_positionable concern.
    Include it on models that have a position column so that records can be manually sequenced.

---
+++ app/models/concerns/is_positionable.rb
@@ -0,0 +1,21 @@
+module Concerns::IsPositionable
+  extend ActiveSupport::Concern
+
+  included do
+    scope :positioned, -> { order(:position) }
+    before_save :set_positions
+  end
+
+
+  def set_positions
+    records = self.class.all.active.positioned.to_a.reject!{ |record| record == self }
+    index = self.position - 1
+    records.insert(index, self).compact!
+
+    records.each_with_index do |record, x|
+      position = x + 1
+      record.update_columns(position: position)
+    end
+  end
+
+end

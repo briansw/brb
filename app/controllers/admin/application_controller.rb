3ac942f6f48bc5a4a81ddfec79af4692ccb092c9
Jack Jennings <j@ckjennin.gs>
2014-06-19 14:35:18 -0700

    scaffold user into dummy app

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,4 +6,17 @@ class Admin::ApplicationController < InheritedResources::Base
   include Admin::Concerns::CRUDMethods
   include Admin::Concerns::Taggings
 
+  protected
+
+  def authenticate_user!
+    unless current_user && current_user.active?
+      redirect_to admin_login_path, notice: 'Please log in'
+    end
+  end
+
+  def current_user
+    @current_user ||= User.where(id: session[:user_id]).first
+  end
+  helper_method :current_user
+
 end

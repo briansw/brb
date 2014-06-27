d28988328ee8d9c05087ee74c6efbd73c2c6e52e
Jack Jennings <j@ckjennin.gs>
2014-06-27 09:34:05 -0700

    uncomment storage option (calling again in subclass overrides option)

--- app/uploaders/admin/attachment_uploader.rb
+++ app/uploaders/admin/attachment_uploader.rb
@@ -1,7 +1,7 @@
 class Admin::AttachmentUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  # storage :file
+  storage :file
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"

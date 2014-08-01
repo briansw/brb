fd62c6282bbd704cba15d3123632ddcbfac2d29f
Jack Jennings <j@ckjennin.gs>
2014-08-01 16:26:57 -0700

    remove bang methods

--- app/models/content_block.rb
+++ app/models/content_block.rb
@@ -3,8 +3,8 @@ class ContentBlock < ActiveRecord::Base
   belongs_to :parent, polymorphic: true
 
   @@block_types = Dir.glob("#{Rails.root}/app/models/*_block.rb").collect do |block|
-        block.gsub!(/.*\//, '').gsub!('.rb', '').to_sym
-      end
+    block.gsub(/.*\//, '').gsub('.rb', '').to_sym
+  end
   
   @@block_types.each do |block_type|
     has_one block_type, dependent: :destroy

3268a7543cd7a7df80517ca1f2b4cf6f18c6b8f1
Brian Watterson <briansw@gmail.com>
2014-08-01 19:05:29 -0400

    build the block list from model files ending in _block.rb

--- app/models/content_block.rb
+++ app/models/content_block.rb
@@ -1,15 +1,8 @@
 class ContentBlock < ActiveRecord::Base
 
   belongs_to :parent, polymorphic: true
-  
-  @@block_types = [:text_block,
-                   :image_block,
-                   :video_block,
-                   :slideshow_block,
-                   :gist_block,
-                   :interview_block,
-                   :section_block
-                  ]
+
+  @@block_types = block_list_from_files
   
   @@block_types.each do |block_type|
     has_one block_type, dependent: :destroy
@@ -23,6 +16,14 @@ class ContentBlock < ActiveRecord::Base
   def as_json(options = {})
     options.reverse_merge! only: [:id], include: @@block_types
     super(options)
+  end
+
+  def self.block_list_from_files
+    blocks = Dir.glob("#{Rails.root}/app/models/*_block.rb")
+
+    blocks.collect do |block|
+      block.gsub!(/.*\//, '').gsub!('.rb', '').to_sym
+    end
   end
 
 end

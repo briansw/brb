32126276f1315c7533589ea16c6796926173aa2b
Brian Watterson <briansw@gmail.com>
2014-09-24 13:35:36 -0400

    attemp to make sluggable accept a list of options to go thru

--- app/models/concerns/sluggable.rb
+++ app/models/concerns/sluggable.rb
@@ -1,15 +1,38 @@
 module Concerns::Sluggable
   extend ActiveSupport::Concern
 
+  included do
+    cattr_accessor :sluggable_options do
+      []
+    end
+  end
+
   module ClassMethods
-    
-    def sluggable(name)
+
+    def sluggable(*fields)
+      fields.each { |f|  self.sluggable_options << f }
       before_save :generate_slug
 
       class_eval <<-CODE, __FILE__, __LINE__ + 1
+
         def generate_slug
-          self.slug = self.#{name}.mb_chars.normalize(:kd)
+          sluggable_options.collect! do |field|
+            slugify(field)
+          end
+
+          self.slug = sluggable_options.collect{ |f| f.present? }.first
+
+        end
+
+        def slugify(field)
+
+puts field
+puts self.send(field).present?
+
+          if self.respond_to?(field) && self.send(field).present?
+            self.send(field).mb_chars.normalize(:kd)
             .gsub(/[^\x00-\x7F]/n,'').parameterize.gsub('_', '')
+          end
         end
 
         def self.from_param(param)

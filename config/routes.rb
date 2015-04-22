738a59a92fbc7889b2c48f64303eaeab1be0a6e0
Jack Jennings <jack@remote.gd>
2015-04-22 11:12:16 -0700

    add admin_for :images to gem routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  
+
+  admin_for :images, private: true
+
   namespace :admin do
     get  'login',  to: 'sessions#new'
     post 'login',  to: 'sessions#create'
     get  'logout', to: 'sessions#destroy'
-    
+
     resource :images
   end
-  
+
   get 'login', to: redirect('/admin/login')
-  
+
 end

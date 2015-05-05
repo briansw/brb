33674564fc81b7cfb9dc8ff72516b4f00942e09b
Jack Jennings <jack@remote.gd>
2015-05-05 13:20:47 -0700

    remove double admin image routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
     get  'login',  to: 'sessions#new'
     post 'login',  to: 'sessions#create'
     get  'logout', to: 'sessions#destroy'
-
-    resource :images
   end
 
   get 'login', to: redirect('/admin/login')
